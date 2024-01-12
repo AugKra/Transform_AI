3841093e64444c7cdd9e58bf50c61165205af4c3
AugKra <131650115+AugKra@users.noreply.github.com>
2024-01-12 16:12:00 +0100

    clean up

--- home.py
+++ home.py
@@ -6,7 +6,7 @@ credentials_path = "/Users/august/Desktop/acquired-subset-407621-d657e5b55854.js
 set_up_authentication(credentials_path)
 
 app = Flask(__name__)
-app.secret_key = "your_secret_key"  # Replace with your actual secret key
+app.secret_key = "your_secret_key"  
 
 @app.route('/')
 def hello_world():
@@ -16,18 +16,15 @@ def hello_world():
 @app.route('/plain_text', methods=['POST', 'GET'])
 def plain_text():
     if request.method == 'GET':
-        # Retrieve the improved text from the session
         improved_text = session.get('improved_text', None)
 
         if improved_text is None:
-            # If no improved text is found in the session, redirect to the home page
             return redirect('/')
 
         detected_text = session.get('detected_text', None)
 
         return render_template('plain_text.html', detected_text=detected_text)
 
-    # Handle file upload and processing for plain text
     result = handle_file_upload_and_processing(request, 'plain_text')
 
     if result is not None:
@@ -39,18 +36,15 @@ def plain_text():
 @app.route('/mind_map', methods=['POST', 'GET'])
 def mind_map():
     if request.method == 'GET':
-        # Retrieve the improved text from the session
         improved_text = session.get('improved_text', None)
 
         if improved_text is None:
-            # If no improved text is found in the session, redirect to the home page
             return redirect('/')
 
         key_concepts = extract_key_concepts(improved_text)
 
         return render_template('mind_map.html', key_concepts=key_concepts)
 
-    # Handle file upload and processing for mind map
     result = handle_file_upload_and_processing(request, 'mind_map')
 
     if result is not None:
@@ -62,18 +56,15 @@ def mind_map():
 @app.route('/quiz', methods=['POST', 'GET'])
 def generate_quiz():
     if request.method == 'GET':
-        # Retrieve the improved text from the session
         improved_text = session.get('improved_text', None)
 
         if improved_text is None:
-            # If no improved text is found in the session, redirect to the home page
             return redirect('/')
 
         quiz_data = generate_quiz_data(improved_text)
 
         return render_template('quiz.html', quiz_data=quiz_data)
 
-    # Handle file upload and processing for quiz
     result = handle_file_upload_and_processing(request, 'generate_quiz')
 
     if result is not None:
@@ -92,26 +83,20 @@ def handle_file_upload_and_processing(request, action):
         return redirect(request.url)
 
     if action == 'plain_text':
-        # Call the function to process the uploaded image for text extraction
         detected_text = process_uploaded_image(file)
 
-        # Generate key concepts for mind map
         improved_text = generate_lm_text(detected_text)
 
-        # Store the improved text and detected text in the session
         session['improved_text'] = improved_text
         session['detected_text'] = detected_text
 
         return redirect('/plain_text')
 
     elif action == 'mind_map':
-        # Call the function to process the uploaded image for mind map creation
         detected_text = process_uploaded_image(file)
 
-        # Generate key concepts for mind map
         improved_text = generate_lm_text(detected_text)
 
-        # Store the improved text and detected text in the session
         session['improved_text'] = improved_text
         session['detected_text'] = detected_text
 
@@ -123,7 +108,6 @@ def handle_file_upload_and_processing(request, action):
 
         improved_text = generate_lm_text(detected_text)
 
-        # Store the improved text in the session
         session['improved_text'] = improved_text
 
         return redirect('/quiz')
--- utils/ocr_test2.py
+++ utils/ocr_test2.py
@@ -22,7 +22,6 @@ def detect_text(image_path):
     texts = response.text_annotations
 
     if texts:
-        # Print only the first detected text annotation
         print(f"Detected text: {texts[0].description}")
         return texts[0].description
     else:
@@ -31,10 +30,9 @@ def detect_text(image_path):
 
 
 def generate_lm_text(ocr_text):
-    # Use OpenAI API to generate improved text
     prompt = f"Write a well formulated text based on the following unstructered text, originally handwritten and digitized through OCR. Use continuous prose without any bullet points or similar formatting. If interpretation is needed due to OCR errors, please provide the best approximation. Reply with only the transcribed text:\n\n{ocr_text}"
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-1106",  # Use the desired chat model
+        model="gpt-3.5-turbo-1106", 
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content": prompt},
@@ -48,11 +46,9 @@ def generate_lm_text(ocr_text):
 
 
 def process_uploaded_image(file):
-    # Save the uploaded file
     file_path = "uploads/" + secure_filename(file.filename)
     file.save(file_path)
 
-    # Call the OCR function from ocr_test2.py
     detected_text = detect_text(file_path)
 
     improved_text = generate_lm_text(detected_text)
@@ -64,7 +60,6 @@ def process_uploaded_image(file):
     return improved_text
 
 def extract_key_concepts(improved_text):
-    # Use OpenAI API to extract key concepts from improved_text
     prompt = f"Generate a concise list of key concepts for a mind map based on the following text. Present each concept on a separate line without using dashes, bullet points, or any additional characters. Use only one to three words per concept. Emphasize brevity and clarity.\n\n{improved_text}"
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-1106",
@@ -84,11 +79,10 @@ def generate_quiz_data(improved_text):
     print("Generating quiz data...")
     quiz_data = []
     
-    # Use OpenAI API to generate quiz questions and answers based on improved_text
     try:
         prompt = f"Generate a quiz question and answer based on the following text:\n\n{improved_text}\n\nQuestion 1:"
         response = openai.Completion.create(
-            engine="gpt-3.5-turbo-instruct",  # Choose the appropriate engine for quiz generation
+            engine="gpt-3.5-turbo-instruct", 
             prompt=prompt,
             temperature=0.7,
             max_tokens=500,
@@ -96,10 +90,8 @@ def generate_quiz_data(improved_text):
         )
         generated_data = response['choices'][0]['text'].strip()
 
-        # Split the generated data into questions and answers
         qa_pairs = [pair.strip() for pair in generated_data.split('Answer') if pair.strip()]
 
-        # Create a list of dictionaries with 'question' and 'answer'
         for i in range(0, len(qa_pairs), 2):
             question = qa_pairs[i]
             answer = qa_pairs[i + 1] if i + 1 < len(qa_pairs) else ''
